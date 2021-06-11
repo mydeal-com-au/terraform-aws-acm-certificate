1b67b451682b845988ecdd59fd74c3f7ba54862c
Allan Denot <adenot@gmail.com>
2021-06-11 02:28:06 +0000

    Lint fixes

--- _variables.tf
+++ _variables.tf
@@ -4,10 +4,10 @@ variable "domain_names" {
 
 variable "validation_method" {
   description = "DNS, EMAIL or NONE"
-  default = "DNS"
+  default     = "DNS"
 }
 
 variable "hosted_zone_id" {
   description = "Route53 hosted zone to create validation records. Must be set when validation_method is DNS"
-  default = ""
+  default     = ""
 }
