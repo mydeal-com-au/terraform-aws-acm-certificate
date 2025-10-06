2fd33db3ff67a4b7b582cb941485264d7a524855
Javad Khan <jkhan4@woolworths.com.au>
2025-10-07 10:37:25 +1100

    Flag to allow cert export

--- certificate.tf
+++ certificate.tf
@@ -3,6 +3,13 @@ resource "aws_acm_certificate" "cert" {
   subject_alternative_names = slice(var.domain_names, 1, length(var.domain_names))
   validation_method         = var.validation_method
 
+  dynamic "options" {
+    for_each = var.export ? [1] : []
+    content {
+      export = "ENABLED"
+    }
+  }
+
   lifecycle {
     create_before_destroy = true
   }
